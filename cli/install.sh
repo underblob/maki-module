4bad05f98f8eb70725f804c83c9ae165e3ed9b41
Brad Barwick <underblob@gmail.com>
2019-08-16 23:27:48 -0700

    Install command:
    * Add error handler functions: `helpers/err.sh`
    * Add React example template.
    * Stub `template/README.md`.
    * Update `cli/install.sh` to copy example templates.

--- cli/install.sh
+++ cli/install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 source ./helpers/color.sh
+source ./helpers/err.sh
 
 color_init
 
-CWD=$1
+PKG_DIR=$(pwd)
+CWD_DIR=$1
 
 color "${BLUE}INSTALL TEMPLATES"
 color
-color "${BLUE}Copying files to ${YELLOW}${CWD}/templates"
+
+color "${BLUE}Copy templates to ${YELLOW}${CWD_DIR}/templates"
+[ -d "${CWD_DIR}/templates" ] && color "${RED}Folder already exists." && err_abort 1
+cp -r "${PKG_DIR}/templates" "${CWD_DIR}/"
+err_abort $?
+
+color "${BLUE}Open folder"
+open "${CWD_DIR}/templates"
+
+color
+color "${GREEN}Success!"
 color
---
+++ helpers/err.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+err_message() {
+  color "${RED}An operation failed with error code ${1}\n"
+}
+
+err_warn() {
+  [ $1 -ne 0 ] && err_message $1
+}
+
+err_abort() {
+  [ $1 -ne 0 ] && err_message $1 && exit 0
+}
